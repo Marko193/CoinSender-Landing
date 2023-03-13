0b1c1f386f97d8e3a1e984d7d0afa540528ed72b
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-13 22:26:40 +0300

    feat: added components folder with necessary structure

---
+++ src/components/UserFriendlyFeatures/index.js
@@ -0,0 +1,8 @@
+import styles from "./user-styles.module.css"
+export default function UserFriendlyFeatures() {
+    return (
+        <>
+            <div className={styles.test}>User</div>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-
+import UserFriendlyFeatures from "@/components/UserFriendlyFeatures";
 
 export default function Home() {
   return (
@@ -12,8 +10,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>Main content</div>
+      <main>
+          <UserFriendlyFeatures/>
       </main>
     </>
   )
