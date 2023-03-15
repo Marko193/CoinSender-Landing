6f3fa639e77dcabb44b4e94663d937042f5e0baf
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-15 03:21:11 +0300

    style: add delimteres with grid styles (in progress)

--- src/components/CoinSenderBenefits/index.js
+++ src/components/CoinSenderBenefits/index.js
@@ -8,6 +8,139 @@ export default function CoinSenderBenefits() {
                 title="Discover the Benefits of Using CoinSender for Your Cryptocurrency Payouts"
                 description="When it comes to multi-transaction payouts in cryptocurrency, CoinSender is the platform of choice for businesses and projects of all sizes."
             />
+            <div className={styles.content_container}>
+                <div className={styles.content} id={styles.first_row}>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                            <div className={styles.separator}/>
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                        </div>
+                        <div className={styles.separator}/>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                            <div className={styles.separator}/>
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                        </div>
+                        <div className={styles.separator}/>
+                    </div>
+                </div>
+                <div className={styles.content} id={styles.second_row}>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration 123</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.block}>
+                        <div className={styles.sub_block}>
+                            <div className={styles.title}>Wallet Integration</div>
+                            <div className={styles.description}>CoinSender uses funds allocated directly in your Web3
+                                wallet, so you can easily send funds directly to your recipient's wallets.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*<div className={styles.benefits_layout}>*/}
+            {/*    <div className={styles.layout_column}>*/}
+            {/*        <div className={styles.benefit_block} style={{borderRight: '1px solid green', paddingTop: "43px"}}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.layout_column}>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.layout_column}>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.layout_column}>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        <div className={styles.benefit_block}>*/}
+            {/*            <div className={styles.block_title}>Wallet Integration</div>*/}
+            {/*            <div className={styles.block_description}>CoinSender uses funds allocated directly in your Web3*/}
+            {/*                wallet, so you can easily send funds directly to your recipient's wallets.*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     )
 }
