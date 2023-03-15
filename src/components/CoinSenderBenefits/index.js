40be6baaf3cafb126107e84d69f341ceebc58158
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-15 13:46:56 +0300

    feat: added try now button

--- src/components/CoinSenderBenefits/index.js
+++ src/components/CoinSenderBenefits/index.js
@@ -89,6 +89,7 @@ export default function CoinSenderBenefits() {
                     </div>
                 </div>
             </div>
+            <button className={styles.sign_up_button}>Try now</button>
         </div>
     )
 }
