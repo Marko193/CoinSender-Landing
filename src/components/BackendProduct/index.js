26d4d2eb24b011b9b97ab88be3858f9b05a51990
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-16 20:45:51 +0300

    feat: made second-third animations

--- src/components/BackendProduct/index.js
+++ src/components/BackendProduct/index.js
@@ -3,12 +3,25 @@ import styles from './backend-product.module.scss'
 export default function BackendProduct() {
     return (
         <>
-            <div id={styles.first_avatar_wrapper}>
-                <div className={styles.avatar_layout}>
-                    <img src="/assets/mock_avatars/first.png" alt='avatar' id={styles.first_avatar}/>
-                </div>
-            </div>
             <div className={styles.backend_product_container}>
+                <div className={styles.first_avatar_wrapper}>
+                    <div className={styles.avatar_layout}>
+                        <img src="/assets/mock_avatars/first.png" alt='avatar' id={styles.first_avatar}/>
+                    </div>
+                </div>
+                <div id={styles.second_third_avatars_wrapper}>
+                    <div className={styles.second_third_avatars}>
+                        <div className={styles.second_animation_wrapper}>
+                            <img src="/assets/mock_avatars/second.png" alt='avatar' className={styles.second_avatar}/>
+                        </div>
+                    </div>
+                    <div className={styles.second_third_avatars} id={styles.third_avatar}>
+                        <div className={styles.third_animation_wrapper}>
+                            <img src="/assets/mock_avatars/third.png" alt='avatar'/>
+                        </div>
+                    </div>
+                </div>
+
                 <div className={styles.container_text_wrapper}>
                     <div className={styles.title}>
                         Backed by
