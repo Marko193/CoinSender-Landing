ba03231df4c85ee5971787c13d8431192326949f
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-17 01:30:49 +0300

    feat: working with privacy policy page

--- src/pages/privacy_policy/index.jsx
+++ src/pages/privacy_policy/index.jsx
@@ -1,7 +1,24 @@
+import {Header} from "@/components/Header/header";
+import Footer from "@/components/Footer";
+import styles from './privacy_policy.module.scss'
+
 export default function PrivacyPolicy() {
     return (
-        <div>
-            Privacy policy page
-        </div>
+        <>
+            <Header/>
+            <div className={styles.main_wrapper}>
+                <div className={styles.main_title}>
+                    Privacy policy
+                </div>
+                <div className={styles.blocks_info_wrapper}>
+                    <div className={styles.blocks_info_content}>
+                        <div className={styles.block_title}>
+                            <p>General information</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </>
     )
 }
