42c86f4ecfcb1d2af0d06bb2acf36644bab8eb2a
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-20 22:20:03 +0300

    feat: made stylished accordions with mock text data

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -7,6 +7,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {Header} from "@/components/Header/header";
 import Footer from "@/components/Footer";
 import styles from './faq.module.scss';
+import {ContactForm} from "@/components/contact/contact";
 
 export default function FAQ() {
     return (
@@ -29,6 +30,117 @@ export default function FAQ() {
                                 }
                                 className={styles.accordion_name_block}
                             >
+                                <div className={styles.name_block_title}>Customer Database
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>Is my transaction secure?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>For whom could it be beneficial?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>How do I get started with CoinSender?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>Can I cancel a transaction once it has been
+                                    sent?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>Does CoinSender offer customer support?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender is a platform for distributing tokens and making multi-transaction
+                                    payouts in cryptocurrency. It allows you to send cryptocurrency payments quickly and
+                                    securely.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                    </div>
+                    <div className={styles.accordion_column}>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
                                 <div className={styles.name_block_title}>What cryptocurrencies does <br/> CoinSender
                                     support?
                                 </div>
@@ -41,26 +153,102 @@ export default function FAQ() {
                                 </div>
                             </AccordionDetails>
                         </Accordion>
-                    </div>
-                    <div className={styles.accordion_column}>
-                        <Accordion>
+                        <Accordion className={styles.accordion_main_content}>
                             <AccordionSummary
-                                expandIcon={<ExpandMoreIcon/>}
-                                aria-controls="panel1a-content"
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
                                 className={styles.accordion_name_block}
                             >
-                                <Typography>Accordion 1</Typography>
+                                <div className={styles.name_block_title}>How much does CoinSender cost?
+                                </div>
                             </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender supports a wide range of popular cryptocurrencies, including Bitcoin,
+                                    Ethereum, and many others. Check out the platform for the full list of supported
+                                    cryptocurrencies.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>Can I use CoinSender for personal transactions?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender supports a wide range of popular cryptocurrencies, including Bitcoin,
+                                    Ethereum, and many others. Check out the platform for the full list of supported
+                                    cryptocurrencies.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>What is the maximum number of wallets that I
+                                    can send tokens to in one transaction?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender supports a wide range of popular cryptocurrencies, including Bitcoin,
+                                    Ethereum, and many others. Check out the platform for the full list of supported
+                                    cryptocurrencies.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>How long does it take for a transaction to be
+                                    processed?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender supports a wide range of popular cryptocurrencies, including Bitcoin,
+                                    Ethereum, and many others. Check out the platform for the full list of supported
+                                    cryptocurrencies.
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion className={styles.accordion_main_content}>
+                            <AccordionSummary
+                                expandIcon={
+                                    <ExpandMoreIcon className={styles.expand_more_icon}/>
+                                }
+                                className={styles.accordion_name_block}
+                            >
+                                <div className={styles.name_block_title}>Is CoinSender available in my country?
+                                </div>
+                            </AccordionSummary>
+                            <AccordionDetails classes={{root: styles.accordion_details_root}}>
+                                <div className={styles.details_content}>
+                                    CoinSender supports a wide range of popular cryptocurrencies, including Bitcoin,
+                                    Ethereum, and many others. Check out the platform for the full list of supported
+                                    cryptocurrencies.
+                                </div>
                             </AccordionDetails>
                         </Accordion>
                     </div>
                 </div>
             </div>
+            <ContactForm/>
             <Footer/>
         </>
     )
