b3606c549557606ef263a19b1417b82b806231f0
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-21 21:11:17 +0300

    refactor: desktop header items list

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,54 +1,75 @@
-import { useState } from "react";
-import { BurgerMenu } from "../burger-menu/burger-menu";
+import {useState} from "react";
+import {BurgerMenu} from "../burger-menu/burger-menu";
 import styles from "./header.module.css";
 import Logo from "../../assets/images/logo-header.svg";
 import Image from "next/image";
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export const Header = () => {
-  const [isActive, setIsActive] = useState(false);
+    const router = useRouter();
+    const [isActive, setIsActive] = useState(false);
 
-  const items = [
-    { name: "Home", link: "#home" },
-    { name: "Features", link: "#features" },
-    { name: "How it works", link: "#howitworks" },
-    { name: "Benefits", link: "#benefits" },
-    { name: "Pricing", link: "#pricing" },
-    { name: "Intagrations", link: "#integrations" },
-    { name: "Roadmap", link: "#roadmap" },
-    { name: "FAQ", link: "#faq" },
-  ];
+    const items = [
+        {name: "Home", link: "#home"},
+        {name: "Features", link: "#features"},
+        {name: "How it works", link: "#howitworks"},
+        {name: "Benefits", link: "#benefits"},
+        {name: "Pricing", link: "#pricing"},
+        {name: "Intagrations", link: "#integrations"},
+        {name: "Roadmap", link: "#roadmap"},
+        {name: "FAQ", link: "#faq"},
+    ];
 
-  return (
-    <header className={styles.header}>
-      <div className={styles.header__container + " container"}>
-        <div>
-          <Link href="/">
-            <Image src={Logo} alt="" />
-          </Link>
-        </div>
-        <div className={styles.header__desktop_menu}>
-          <ul className={styles.header__nav}>
-            {items.map(({ name, link }, index) => (
-              <li key={index}>
-                <Link href={"/" + link}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.header__signup}>
-          <Link href="https://transfer.coinsender.io/auth">
-            <button className="button">Sign Up</button>
-          </Link>
-        </div>
-        <div
-          onClick={() => setIsActive(!isActive)}
-          className={`${styles.button_menu} ${isActive ? styles.active : ""}`}
-        >
-          <span></span>
-        </div>
-      </div>
-      <BurgerMenu isActive={isActive} setIsActive={setIsActive} items={items} />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__container + " container"}>
+                <div>
+                    <Link href="/">
+                        <Image src={Logo} alt=""/>
+                    </Link>
+                </div>
+                <div className={styles.header__desktop_menu}>
+                    <ul className={styles.header__nav}>
+                        <li>
+                          <Link href={`/#home`}>Home</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#features`}>Features</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#howitworks`}>How it works</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#benefits`}>Benefits</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#pricing`}>Pricing</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#integrations`}>Integrations</Link>
+                        </li>
+                        <li>
+                            <Link href={`/#roadmap`}>Roadmap</Link>
+                        </li>
+                        <li onClick={() => router.push(`/faq`)}>
+                            <Link href={`/faq`}>FAQ</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.header__signup}>
+                    <Link href="https://transfer.coinsender.io/auth">
+                        <button className="button">Sign Up</button>
+                    </Link>
+                </div>
+                <div
+                    onClick={() => setIsActive(!isActive)}
+                    className={`${styles.button_menu} ${isActive ? styles.active : ""}`}
+                >
+                    <span></span>
+                </div>
+            </div>
+            <BurgerMenu isActive={isActive} setIsActive={setIsActive} items={items}/>
+        </header>
+    );
 };
